42029c4cf06c4b8cf303c181f742ae083d392cf2
Sofia <itssonyamisyura@gmail.com>
2026-02-16 14:45:22 +0100

    first commit

---
+++ dist/script.js
@@ -0,0 +1,201 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/lib/core.js"
+/*!****************************!*\
+  !*** ./src/js/lib/core.js ***!
+  \****************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// (() => {
+//     const $ = function(selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+
+//         obj.hide = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = '';
+//                 // браузер поставит по умолчания(block, flex)
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+//     };
+
+//     window.$ = $;
+// })();
+
+// Быстрый доступ к элементам на странице - $
+// Создаем замыкание при помощи анонимной самовызыв. функции - chaining
+// получаем глобальную функцию, которую можем вызывать по одному символу
+
+const $ = function (selector) {
+  return new $.prototype.init(selector);
+};
+$.prototype.init = function (selector) {
+  if (!selector) {
+    return this; //{}
+  }
+  Object.assign(this, document.querySelectorAll(selector)); // добавляет свойства в объект 
+  this.length = document.querySelectorAll(selector).length;
+  return this;
+};
+
+// второй прототип относится к объекту, который возвращает метод return this --> теперь на объекте const $ мы можем использовать любые методы, которые будут внутри прототипа функции $.prototype.init
+$.prototype.init.prototype = $.prototype;
+window.$ = $;
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ($);
+
+/***/ },
+
+/***/ "./src/js/lib/lib.js"
+/*!***************************!*\
+  !*** ./src/js/lib/lib.js ***!
+  \***************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./core */ "./src/js/lib/core.js");
+/* harmony import */ var _modules_display__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/display */ "./src/js/lib/modules/display.js");
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_core__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+/***/ },
+
+/***/ "./src/js/lib/modules/display.js"
+/*!***************************************!*\
+  !*** ./src/js/lib/modules/display.js ***!
+  \***************************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.show = function () {
+  for (let i = 0; i < this.length; i++) {
+    if (!this[i].style) {
+      continue;
+    }
+    this[i].style.display = '';
+  }
+  return this;
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.hide = function () {
+  for (let i = 0; i < this.length; i++) {
+    if (!this[i].style) {
+      continue;
+    }
+    this[i].style.display = 'none';
+  }
+  return this;
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.toggle = function () {
+  for (let i = 0; i < this.length; i++) {
+    if (!this[i].style) {
+      continue;
+    }
+    if (this[i].style.display === 'none') {
+      this[i].style.display = '';
+    } else {
+      this[i].style.display = 'none';
+    }
+  }
+  return this;
+};
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Check if module exists (development only)
+/******/ 		if (__webpack_modules__[moduleId] === undefined) {
+/******/ 			var e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _lib_lib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/lib */ "./src/js/lib/lib.js");
+
+$('.active').toggle();
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
---
+++ src/js/lib/core.js
@@ -0,0 +1,50 @@
+// (() => {
+//     const $ = function(selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+
+//         obj.hide = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = () => {
+//             elements.forEach(elem => {
+//                 elem.style.display = '';
+//                 // браузер поставит по умолчания(block, flex)
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+//     };
+
+//     window.$ = $;
+// })();
+
+// Быстрый доступ к элементам на странице - $
+// Создаем замыкание при помощи анонимной самовызыв. функции - chaining
+// получаем глобальную функцию, которую можем вызывать по одному символу
+
+const $ = function(selector) {
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function(selector) {
+    if (!selector) {
+        return this; //{}
+    }
+    Object.assign(this, document.querySelectorAll(selector)) // добавляет свойства в объект 
+    this.length = document.querySelectorAll(selector).length;
+
+    return this;
+}; 
+
+// второй прототип относится к объекту, который возвращает метод return this --> теперь на объекте const $ мы можем использовать любые методы, которые будут внутри прототипа функции $.prototype.init
+$.prototype.init.prototype = $.prototype;
+
+window.$ = $;
+
+export default $;

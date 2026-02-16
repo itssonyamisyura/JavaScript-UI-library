2b61f87a61f25954a5ab8d2ce80a9eeb50e3b843
Sofia <itssonyamisyura@gmail.com>
2026-02-16 20:57:09 +0100

    methods for working with elements

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,32 +1,61 @@
 import $ from '../core';
 
-$.prototype.on = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this; // do nothing
-    }
+$.prototype.html = function(content) {
     for (let i = 0; i < this.length; i++) {
-        this[i].addEventListener(eventName, callback);
-    }
-    return this;
-};
-
-$.prototype.off = function(eventName, callback) {
-    if (!eventName || !callback) {
-        return this; 
-    }
-    for (let i = 0; i < this.length; i++) {
-        this[i].removeEventListener(eventName, callback);
-    }
-    return this;
-};
-
-$.prototype.click = function(handler) {
-    for (let i = 0; i < this.length; i++) {
-        if (handler) {
-            this[i].addEventListener('click', handler);
+        if (content) {
+            this[i].innerHTML = content;
         } else {
-            this[i].click(); //if no handler(click()) -> virtual click
+            return this[i].innerHTML;
         }
     }
+
     return this;
 };
+// change/get HTML structure inside element
+
+
+$.prototype.eq = function(i) {
+    const swap = this[i];
+    const objLength = Object.keys(this).length;// кол-во свойств внутри объекта
+
+    for (let i = 0; i < objLength; i++) {
+        delete this[i]; //now collection is empty
+    }
+
+    this[0] = swap;
+    this.length = 1; //insert back only the selected element
+
+    return this;
+};
+// Return only the element with index i from the current collection.
+
+
+$.prototype.index = function() {
+    const parent = this[0].parentNode;
+    const childs = [...parent.children]; //parent.children = html collection --> [...parent.children] = now array has methods
+
+    const findMyIndex = (item) => {
+        return item == this[0]; //one we need
+    }
+
+    return childs.findIndex(findMyIndex); // returns number
+};
+// номер эл по порядку среди сверсников(имеющих одного родителя)
+
+
+
+$.prototype.find = function (selector) {
+    let items = [];
+ 
+    for (let i = 0; i < this.length; i++) {
+        let arr = this[i].querySelectorAll(selector);
+        items.push(...arr);
+        delete this[i];
+    }
+ 
+    Object.assign(this, items);
+    this.length = items.length;
+ 
+    return this;
+};
+// находим элементы среди уже выбранных
